517ec589f99da2513efe7e99dd34f913d3f752e1
new-crystal <oj3339@naver.com>
2023-01-09 18:30:54 +0900

    add axios

--- mobx/src/List.jsx
+++ mobx/src/List.jsx
@@ -1,38 +1,49 @@
-import { useObserver } from "mobx-react"
-import { useRef, useState } from "react";
-import Store from "./store"
+import { useObserver } from "mobx-react";
+import { useEffect, useRef, useState } from "react";
+import Store from "./store";
 
-const List = () =>{
-    const [id, setId] = useState(1)
-    const {listStore} = Store();
-    const inputRef = useRef(null);
+const List = () => {
+  const [id, setId] = useState(3);
+  const { listStore } = Store();
+  const inputRef = useRef(null);
 
-    const onClickAddBtn = () =>{
-        if(inputRef.current.value === ""){
-            return;
-        }
-        const data = {
-            id:id,
-            content: inputRef.current.value
-        }
-        listStore.add(data)
-        setId(id + 1)
-        inputRef.current.value = ""
+  const onClickAddBtn = () => {
+    if (inputRef.current.value === "") {
+      return;
     }
+    const data = {
+      id: id,
+      content: inputRef.current.value,
+    };
+    //listStore.add(data);
+    listStore.postList(data);
+    setId(id + 1);
+    inputRef.current.value = "";
+  };
 
-    const onClickRemoveBtn = (id) =>{
-       listStore.remove(id)
-    }
-    return(
-        <div>
-            {listStore.todoList.map((todo)=>{
-               return (<><h1 key={todo.id}>{todo.content}</h1> <button onClick={()=>onClickRemoveBtn(todo.id)}>-</button></>)
-            })}
-            
-            <input ref={inputRef}/>
-            <button onClick={onClickAddBtn}>+</button>
-        </div>
-    )
-}
+  const onClickRemoveBtn = (id) => {
+    listStore.remove(id);
+  };
 
-export default List
+  useEffect(() => {
+    listStore.getList();
+  }, []);
+  console.log(listStore.todoList);
+  return useObserver(() => (
+    <div>
+      {listStore.todoList.map((todo, i) => {
+        return (
+          <div key={i}>
+            <h1>{todo.content}</h1>{" "}
+            <button onClick={() => onClickRemoveBtn(todo.id)}>-</button>
+          </div>
+        );
+      })}
+
+      <input ref={inputRef} />
+      <button onClick={onClickAddBtn}>+</button>
+    </div>
+  ));
+};
+
+export default List;
--- mobx/src/listStore.jsx
+++ mobx/src/listStore.jsx
@@ -1,13 +1,27 @@
-import { observable } from "mobx";
+import { observable, runInAction } from "mobx";
+import axios from "axios";
 
 const listStore = observable({
-    todoList:[{id:0, content:"열심히 놀기"}],
-    add(todo){
-        this.todoList = [...this.todoList, todo]
-    },
-    remove(id){
-        this.todoList = this.todoList.filter((todo)=>todo.id !== id)
-    }
-})
+  todoList: [],
+  add(todo) {
+    this.todoList = [...this.todoList, todo];
+  },
+  remove(id) {
+    this.todoList = this.todoList.filter((todo) => todo.id !== id);
+  },
+  async getList() {
+    const res = await axios.get("http://localhost:3001/posts");
 
-export default listStore
+    runInAction(() => {
+      this.todoList = res.data;
+    });
+  },
+  async postList(todo) {
+    const res = await axios.post("http://localhost:3001/posts", todo);
+    runInAction(() => {
+      this.todoList = [...this.todoList, res.data];
+    });
+  },
+});
+
+export default listStore;
