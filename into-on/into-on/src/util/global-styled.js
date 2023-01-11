06567272ba68e4694f77a5062331debd9ebf478a
new-crystal <oj3339@naver.com>
2023-01-11 14:04:19 +0900

    edit global style

--- into-on/into-on/src/util/global-styled.js
+++ into-on/into-on/src/util/global-styled.js
@@ -4,10 +4,11 @@ export const GlobalStyle = createGlobalStyle`
 *{
     margin: 0;
     padding: 0;
-    font-family: "Noto Sans KR", NanumSquare, dotum, 돋움, sans-serif;
+   
 }
 body {
     box-sizing: border-box;
+    font-family: "Noto Sans KR", NanumSquare, dotum, 돋움, sans-serif;
 }
 
 h2 {
